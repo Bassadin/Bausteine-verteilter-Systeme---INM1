080274e3b493f9677df0acd91b21c802b31c02b0
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-01 19:04:07 +0200

    some notes and questions

--- Tasks/Task_01/src/main/scala/Main.scala
+++ Tasks/Task_01/src/main/scala/Main.scala
@@ -1,12 +1,23 @@
 import akka.actor.typed.ActorSystem
+
+/*
+
+Unanswered questions:
+- How to pass actor references into other actors (wrapper objects?)
+- How to connect to h2 db?
+
+*/
+
 object Main extends App {
     // val fileParser: ParseFileActor = new ParseFileActor();
     // fileParser.parseFileFrom("E:/Downloads/Aufgabenblatt+01/test_ticks.csv");
 
-    println("starting...")
-    val actor = ActorSystem(ParseFileActor(), "fileParser");
+    println("starting...");
+    val parseFileActor = ActorSystem(ParseFileActor(), "fileParser");
+    val dbConnectorActor = ActorSystem(DatabaseConnectorActor(), "databaseConnector");
+    val convertDataActor = ActorSystem(ConvertDataActor(dbConnectorActor), "fileParser");
 
-    actor ! "E:/Downloads/Aufgabenblatt+01/test_ticks.csv";
+    parseFileActor ! "E:/Downloads/Aufgabenblatt+01/test_ticks.csv";
 
-    println("terminating...")
+    println("terminating...");
 }
