d585ee4de0a6269daabbd5c837185889338b4293
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-01 10:54:16 +0200

    dummy code for db connector actor

--- Tasks/Task_01/src/main/scala/DatabaseConnectorActor.scala
+++ Tasks/Task_01/src/main/scala/DatabaseConnectorActor.scala
@@ -1,3 +1,21 @@
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
 class DatabaseConnectorActor {
-  
+    def connectToDB(): Unit = {
+        // TODO connect to the the h2 db
+
+    }
+
+    def storeInDB(newTick: Tick): Boolean = {
+        // TODO store the tick in the h2 db
+
+        return true;
+    }
+
+    def apply(): Behavior[Tick] = {
+        Behaviors.receive((context, message) => {
+            storeInDB(message);
+            Behaviors.same;
+        })
+    }
 }
