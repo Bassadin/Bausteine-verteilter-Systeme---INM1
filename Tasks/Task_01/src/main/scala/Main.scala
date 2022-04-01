5c69c25d04ad3bfb15e57abb89227c4e2eec2335
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-01 21:17:32 +0200

    use trait for parse file actor

--- Tasks/Task_01/src/main/scala/Main.scala
+++ Tasks/Task_01/src/main/scala/Main.scala
@@ -1,19 +1,11 @@
 import akka.actor.typed.ActorSystem
 
-/*
-
-Unanswered questions:
-- How to pass actor references into other actors (wrapper objects?)
-- How to connect to h2 db?
-- data in isngle string instances or as array?
-
-*/
-
 object Main extends App {
     println("starting...");
     val parseFileActor = ActorSystem(ParseFileActor(), "fileParser");
 
-    parseFileActor ! "E:/Downloads/Aufgabenblatt+01/test_ticks.csv";
+    parseFileActor ! ParseFileData("E:/Downloads/Aufgabenblatt+01/test_ticks.csv");
+    parseFileActor ! StopParseFileActor;
 
     println("terminating...");
 }
--- Tasks/Task_01/src/main/scala/ParseFileActor.scala
+++ Tasks/Task_01/src/main/scala/ParseFileActor.scala
@@ -3,29 +3,36 @@ import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.ActorSystem
 
+trait ParseFileActorProtocol
+object StopParseFileActor extends ParseFileActorProtocol;
+case class ParseFileData(newData: String) extends ParseFileActorProtocol;
+
 object ParseFileActor {
     val convertDataActor = ActorSystem(ConvertDataActor(), "fileParser");
 
     def parseFileFrom(path: String): Unit = {
         // https://alvinalexander.com/scala/how-to-open-read-text-files-in-scala-cookbook-examples/
+        // Drop first 4 lines since they're just headers
         for (line <- Source.fromFile(path).getLines.drop(4)) {
             convertDataActor ! DataToConvert(line);
         }
+
+        // Quit the convert data actor afterwards
         convertDataActor ! EndConvertDataActor;
     }
 
-    def apply(): Behavior[String] = {
+    def apply(): Behavior[ParseFileActorProtocol] = {
         Behaviors.receive((context, message) => {
             message match {
-                case "" =>
+                case StopParseFileActor =>
                     context.log.error("Not a valid file path...")
                     context.log.info("Terminating actor...")
                     Behaviors.stopped;
-                case _ =>
+                case ParseFileData(newData) =>
                     context.log.info(
                       "Valid file path: " + message + ". Now parsing..."
                     )
-                    parseFileFrom(message);
+                    parseFileFrom(newData);
                     Behaviors.same;
             }
         })
