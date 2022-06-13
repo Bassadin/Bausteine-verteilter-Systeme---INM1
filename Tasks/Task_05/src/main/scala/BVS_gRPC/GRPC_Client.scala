e876a22b266e18e0db3530b8e54a94a3e9c12f04
Bastian Hodapp <bastianhodapp@gmail.com>
2022-06-13 23:07:02 +0200

    first version of client

--- Tasks/Task_05/src/main/scala/BVS_gRPC/GRPC_Client.scala
+++ Tasks/Task_05/src/main/scala/BVS_gRPC/GRPC_Client.scala
@@ -1,6 +1,7 @@
 package BVS_gRPC
 
 import Dependencies.Tick
+import de.hfu.protos.aufgabe_05_grpc.{AddTickRequest, DbAccessorGrpc, GetTickRequest}
 import de.hfu.protos.hello.{GreeterGrpc, HelloReply, HelloRequest}
 import io.grpc.ManagedChannelBuilder
 
@@ -10,10 +11,32 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
 object GRPC_Client extends App {
-    def addSync(tick: Tick): Boolean = ???
-    def addAsync(tick: Tick): Future[Boolean] = ???
-    def getSync(symbol: String): Tick = ???
-    def getAsync(symbol: String): Future[Tick] = ???
+    def addSync(tick: Tick): Boolean = {
+        val request = AddTickRequest(tick.symbol, tick.timestampString, tick.price)
+        DbAccessorGrpc.blockingStub(channel).addTick(request).success
+    }
+
+    def addAsync(tick: Tick): Future[Boolean] = {
+        val request = AddTickRequest(tick.symbol, tick.timestampString, tick.price)
+        val response = DbAccessorGrpc.stub(channel).addTick(request)
+
+        // TODO: Das kann so nicht richtig sein
+        response.flatMap(response => Future { response.success })
+    }
+
+    def getSync(symbol: String): Tick = {
+        val request = GetTickRequest(symbol)
+        val response = DbAccessorGrpc.blockingStub(channel).getTick(request)
+        Tick(response.symbol, response.timestamp, response.price)
+    }
+
+    def getAsync(symbol: String): Future[Tick] = {
+        val request = GetTickRequest(symbol)
+        val response = DbAccessorGrpc.stub(channel).getTick(request)
+
+        // TODO: Das kann so nicht richtig sein
+        response.flatMap(response => Future { Tick(response.symbol, response.timestamp, response.price) })
+    }
 
     val logger = Logger.getLogger(GRPC_Client.getClass.getName)
 
@@ -23,18 +46,9 @@ object GRPC_Client extends App {
     val channel =
         ManagedChannelBuilder.forAddress(host, port).usePlaintext().asInstanceOf[ManagedChannelBuilder[_]].build()
 
-    val request = HelloRequest("gRPC")
+    logger.info("trying to get a tick synchronously")
 
-    logger.info("try to greet " + request.name + " synchronously...")
+    logger.info(getSync("MC.FR").toString)
 
-    def logResponseCallback(response: Try[HelloReply]): Unit = {
-        response match {
-            case Success(greeting)  => logger.info("Greeting: " + greeting.message)
-            case Failure(exception) => exception.printStackTrace()
-        }
-    }
-
-    GreeterGrpc.stub(channel).sayHello(request).onComplete(logResponseCallback)
-
-    Thread.sleep(5000)
+    logger.info("terminating...")
 }
