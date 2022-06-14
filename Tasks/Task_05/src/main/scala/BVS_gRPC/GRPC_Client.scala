258c6bb3c52dcf0d229423dff80804b12edc1a9e
Bastian Hodapp <bastianhodapp@gmail.com>
2022-06-14 10:22:12 +0200

    Organize client code a little better

--- Tasks/Task_05/src/main/scala/BVS_gRPC/GRPC_Client.scala
+++ Tasks/Task_05/src/main/scala/BVS_gRPC/GRPC_Client.scala
@@ -3,14 +3,14 @@ package BVS_gRPC
 import Dependencies.Tick
 import de.hfu.protos.aufgabe_05_grpc.{AddTickRequest, DbAccessorGrpc, GetTickRequest}
 import de.hfu.protos.hello.{GreeterGrpc, HelloReply, HelloRequest}
-import io.grpc.ManagedChannelBuilder
+import io.grpc.{ManagedChannel, ManagedChannelBuilder}
 
 import java.util.logging.Logger
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Random, Success, Try}
 
-object GRPC_Client extends App {
+object GRPC_Client {
     def addSync(tick: Tick): Boolean = {
         val request = AddTickRequest(tick.symbol, tick.timestampString, tick.price)
         DbAccessorGrpc.blockingStub(channel).addTick(request).success
@@ -38,28 +38,38 @@ object GRPC_Client extends App {
         response.flatMap(response => Future { Tick(response.symbol, response.timestamp, response.price) })
     }
 
-    val logger = Logger.getLogger(GRPC_Client.getClass.getName)
+    val logger: Logger = Logger.getLogger(GRPC_Client.getClass.getName)
 
     val port = 50051
     val host = "localhost"
 
-    val channel =
-        ManagedChannelBuilder.forAddress(host, port).usePlaintext().asInstanceOf[ManagedChannelBuilder[_]].build()
+    val channel: ManagedChannel = ManagedChannelBuilder
+        .forAddress(host, port)
+        .usePlaintext()
+        .asInstanceOf[ManagedChannelBuilder[_]]
+        .build()
 
-    logger.info("trying to get a tick synchronously")
-    logger.info(getSync("MC.FR").toString)
+    def main(args: Array[String]) = {
+        logger.info("trying to get a tick synchronously")
+        logger.info(getSync("MC.FR").toString)
 
-    logger.info("trying to add a tick synchronously")
-    val tickToAdd: Tick = Tick("ABCDEFG_" + Random.nextString(5), "2022-06-13 09:00:01.149000", 9999)
-    logger.info(addSync(tickToAdd).toString)
+        logger.info("trying to add a tick synchronously")
+        val tickToAdd: Tick = Tick("ABCDEFG_" + Random.nextString(5), "2022-06-13 09:00:01.149000", 9999)
+        logger.info(addSync(tickToAdd).toString)
 
-    logger.info("trying to get a tick asynchronously")
-    getAsync("MC.FR").onComplete(resolvedFuture => { logger.info(resolvedFuture.toString) })
+        logger.info("trying to get a tick asynchronously")
+        getAsync("MC.FR").onComplete(resolvedFuture => {
+            logger.info(resolvedFuture.toString)
+        })
 
-    logger.info("trying to add a tick asynchronously")
-    val secondTickToAdd: Tick = Tick("ZYXWV_" + Random.nextString(5), "2022-06-10 09:00:01.149000", 1111)
-    addAsync(secondTickToAdd).onComplete(resolvedFuture => { logger.info(resolvedFuture.toString) })
+        logger.info("trying to add a tick asynchronously")
+        val secondTickToAdd: Tick = Tick("ZYXWV_" + Random.nextString(5), "2022-06-10 09:00:01.149000", 1111)
+        addAsync(secondTickToAdd).onComplete(resolvedFuture => {
+            logger.info(resolvedFuture.toString)
+        })
 
-    Thread.sleep(5000)
-    logger.info("terminating...")
+        Thread.sleep(5000)
+        logger.info("terminating...")
+
+    }
 }
