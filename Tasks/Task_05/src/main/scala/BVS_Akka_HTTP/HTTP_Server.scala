ae318aedec05d7bfa4ce2f74511b2b861f4391e3
Bastian Hodapp <bastianhodapp@gmail.com>
2022-06-14 14:38:27 +0200

    use spray json formatting for server

--- Tasks/Task_05/src/main/scala/BVS_Akka_HTTP/HTTP_Server.scala
+++ Tasks/Task_05/src/main/scala/BVS_Akka_HTTP/HTTP_Server.scala
@@ -1,22 +1,34 @@
 package BVS_Akka_HTTP
 
+import Dependencies.{Tick, TickDatabase}
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.javadsl.Behaviors
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.Directives._
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 import scala.io.StdIn
 
-object HTTP_Server {
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+    implicit val tickFormat: RootJsonFormat[Tick] = jsonFormat3(Tick)
+}
+
+object HTTP_Server extends Directives with JsonSupport {
     def main(args: Array[String]) = {
         implicit val system = ActorSystem(Behaviors.empty, "http-server")
         implicit val executionContext = system.executionContext
 
         val route = {
-            path("hello") {
+            path("getTick") {
                 get {
-                    complete { HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http!</h1>") }
+                    parameters("tickSymbol") { (tickSymbol) =>
+                        println(tickSymbol)
+                        complete { TickDatabase.getTickFromDB(tickSymbol) }
+
+                    }
                 }
             }
         }
