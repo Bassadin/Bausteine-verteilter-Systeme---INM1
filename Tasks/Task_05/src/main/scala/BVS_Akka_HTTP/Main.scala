575255bafaea8d789efe2dbbb80731b337d8ceeb
Bastian Hodapp <bastianhodapp@gmail.com>
2022-06-20 10:49:34 +0200

    remove superfluous files

--- Tasks/Task_05/src/main/scala/BVS_Akka_HTTP/Main.scala
+++
@@ -1,8 +0,0 @@
-package BVS_Akka_HTTP
-
-object Main extends App {
-    // GitHub-Repo: https://github.com/Bassadin/Bausteine-verteilter-Systeme-INM1
-    println("Starting...")
-
-    println("Terminating...")
-}
--- Tasks/Task_05/src/main/scala/BVS_gRPC/Main.scala
+++
@@ -1,8 +0,0 @@
-package BVS_gRPC
-
-object Main extends App {
-    // GitHub-Repo: https://github.com/Bassadin/Bausteine-verteilter-Systeme-INM1
-    println("Starting...")
-
-    println("Terminating...")
-}
